db35579acefd98c05ae2a4c1d4a9bb59d8f06152
Aaron Rizzuto <aaron.rizzuto23@gmail.com>
2020-08-27 14:33:47 -0500

    added instructions to example.py

--- example.py
+++ example.py
@@ -1,56 +1,22 @@
 import Comove
 
-targname="TYC 6110-1007-1"
+## Define the inputs:
+##An target star name searchable on simbad. If coordinates are used, it will just be used as the default results directory
+targname="TYC 6110-1007-1" 
+
+##Alternative is to use coordinates, default is [None,None] in which case the targname is used to get coordinates
 rd = [None,None]
-rd = ['12 40 45.9694160957', '-21 52 21.831891557']
+#rd = ['12 40 45.9694160957', '-21 52 21.831891557']
 
-radvel=-15.12
+##input target star radial velocity to calulate 3D space velocities
+radvel=-15.12 ##km/s
 
-vlim=5.0
-srad=25.0
-
-
-output_location = Comove.findfriends(targname,radvel,velocity_limit=vlim,search_radius=srad,radec=rd,output_directory=None)
-#print('output is in ' + output_location)
-#print('Done')
+##Neighbour velocity difference limit, and on sky search radius
+vlim=5.0 ##km/s
+srad=25.0 ##degrees
 
 
+##This line runs the entire code. Set showplots=True to interactively plot, otherwise they are only saved as pngs
+##Set verbose=True to see LOTS of print output
+output_location = Comove.findfriends(targname,radvel,velocity_limit=vlim,search_radius=srad,radec=rd,output_directory=None,verbose=False,showplots=False)
 
-##Example Target Names and radial velocities
-    # Search targets
-    #targname="HR 8799"
-    #radvel = -13.0 * u.kilometer / u.second
-    #targname="HD 110082"                        # Faux-Octans planet
-    #radvel = 3.63 * u.kilometer / u.second
-    #targname="V1298 Tau"
-    #radvel = 16.15 * u.kilometer / u.second
-    #targname="Kappa And"
-    #radvel = -12.7 * u.kilometer / u.second
-    #targname="HD 63433"                        # UMa planet
-    #radvel = -15.81 * u.kilometer / u.second
-    #targname='HD 107700'                        # Middle of Coma Ber cluster
-    #radvel= 0.5 * u.kilometer / u.second
-    #targname="HD 984"
-    #radvel = 0.55 * u.kilometer / u.second
-    #targname="TYC 7520-00369-1"
-    #radvel = -2.33 * u.kilometer / u.second
-    #targname="TYC 5909-00319-1"
-    #radvel = 23.68 * u.kilometer / u.second
-    #targname="HIP 73765"
-    #radvel = -12.10 * u.kilometer / u.second
-    #targname="K2-284"
-    #radvel = 16.96 * u.kilometer / u.second
-    #targname="Kepler-51"
-    #radvel = -4.3 * u.kilometer / u.second
-    #targname="TYC 4337-00128-1"
-    #radvel = -9.21 * u.kilometer / u.second
-    #targname="HD 92487"
-    #radvel = 0.0 * u.kilometer / u.second # Guess for initial run, no RV available
-    #targname="TYC 3496-1082-1"
-    #radvel = -7.36 * u.kilometer / u.second
-    #targname="HD 269132"                       # NGC 1901 central star
-    #radvel = 1.43 * u.kilometer / u.second
-    #targname="HIP 65469"
-    #radvel = -7.33 * u.kilometer / u.second
-    #targname="TYC 6110-1007-1"
-    #radvel = -15.12 * u.kilometer / u.second
